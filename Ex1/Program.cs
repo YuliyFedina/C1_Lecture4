a4bcff152708a18a3b92962510568540d8a351a4
Федина Юлия <yu.belova@2gis.ru>
2019-06-13 23:50:02 +0700

    Задания для 4 лекции

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Ex1
 {
+    //Создать класс “Телефон”, который содержит следующие данные: 
+    //код города, номер телефона, которые не могут быть изменены.
+    //Добавить метод, который возвращает строку вида “(999) 999999” или “999999”,
+    //если код города отсутствует.
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World11!");
+            var number1 = new Telephone("999", "999999");
+            Console.WriteLine(number1.OutputPhone());
+            var number2 = new Telephone(null, "999999");
+            Console.WriteLine(number2.OutputPhone());
+        }
+    }
+    
+    public class Telephone
+    {
+        public readonly string PhoneCode;
+        public readonly string PhoneNubmer;
+
+        public Telephone(string phoneCode, string phoneNumber)
+        {
+            PhoneCode = phoneCode;
+            PhoneNubmer = phoneNumber;
+        }
+
+        public string OutputPhone()
+        {
+            if (PhoneCode == null)
+            {
+                return PhoneNubmer;
+            }
+            else
+            {
+                return '(' + PhoneCode + ')' + PhoneNubmer;
+            }
         }
     }
 }
---
+++ Ex2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ex2
+{
+    //Преобразовать класс “Телефон” на работу со свойствами
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = new Telephone("999", "999999");
+            Console.WriteLine(number1.OutputPhone());
+            var number2 = new Telephone(null, "999999");
+            number2.OutputPhone();
+            Console.WriteLine(number2.OutputPhone());
+        }
+    }
+    public class Telephone
+    {
+        private readonly string _phoneCode;
+        private readonly string _phoneNumber;
+
+        public string PhoneCode
+        {
+            get
+            {
+                return _phoneCode;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+        }
+
+        public Telephone(string phoneCode, string phoneNumber)
+        {
+            _phoneCode = phoneCode;
+            _phoneNumber = phoneNumber;
+        }
+
+        public string OutputPhone()
+        {
+            if (PhoneCode == null)
+            {
+                return PhoneNumber;
+            }
+            else
+            {
+                return '(' + PhoneCode + ')' + PhoneNumber;
+            }
+        }
+    }
+}
